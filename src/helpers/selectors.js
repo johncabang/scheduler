08267534cd70c22e9c322247728407be67c57121
John Cabang <johncabz@gmail.com>
2020-10-29 04:46:03 -0400

    Add helpers folder, includes selectors function w/ unit test file

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+  // console.log(state.days);
+  const results = [];
+  const filteredDays = state.days.filter((d) => d.name === day);
+  // console.log(filteredDays);
+  if (filteredDays.length === 0) return results;
+  for (const app of filteredDays[0].appointments) {
+    // console.log(filteredDays[0].appointments);
+    // console.log(app);
+    results.push(state.appointments[app]);
+  }
+  return results;
+}
